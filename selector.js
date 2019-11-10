e93665b9e525ec1d1ed917726e005756dc3e05aa
Wadgma Masab <wadgmamasab@gmail.com>
2019-11-10 04:51:00 -0500

    update

--- selector.js
+++ selector.js
@@ -13,3 +13,15 @@ function deselectAll() {
         indivResults[i].children[0].checked = false;
     }
 }
+function countClicks() {
+  if (typeof(Storage) !== "undefined") {
+    if (localStorage.clickcount) {
+      localStorage.clickcount = Number(localStorage.clickcount)+1;
+    } else {
+      localStorage.clickcount = 1;
+    }
+    document.getElementById("clicks").innerHTML = localStorage.clickcount;
+  } else {
+    document.getElementById("clicks").innerHTML = "Sorry, your browser does not support web storage...";
+  }
+}
